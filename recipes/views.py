966493a7488aaf3f367f18cabfbb823854c8e2b1
Raimundo Neto <rfmeloneto@gmail.com>
2022-12-29 15:44:19 -0300

    factory recipes for teste home

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .util import recipe_factory as rf
+
 
 def home(request):
-    return render(request , 'recipes/pages/home.html')
+    return render(request , 'recipes/pages/home.html', context={
+        'recipes':[rf.make_recipe() for _ in range(10)]
+    })
 
 
 def recipes(request,id):
-    return render(request, 'recipes/pages/recipe-view.html' )
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': rf.make_recipe()
+    })
