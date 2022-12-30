4904afa97bd000ef53acd319388f9b68cc0477a1
Raimundo Neto <rfmeloneto@gmail.com>
2022-12-30 20:45:04 -0300

    admin created with category

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from recipes.models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
