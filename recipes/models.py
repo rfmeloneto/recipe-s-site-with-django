2a1da365546a16fd4d6c22ffcd7bcdb456137409
Raimundo Neto <rfmeloneto@gmail.com>
2022-12-30 20:06:04 -0300

    fk author and category

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class recipe(models.Model):
+class Category (models.Model):
+    name = models.CharField(max_length=65)
+
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
@@ -14,3 +18,6 @@ class recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+    
