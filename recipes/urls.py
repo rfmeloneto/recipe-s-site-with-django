cd451083ef2c93518157d68f521a8c6b0ad7bcdc
Raimundo Neto <rfmeloneto@gmail.com>
2022-12-30 19:53:17 -0300

    Criação dos primeiros models

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from recipes import views
 
+app_name='recipes'
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes)
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipes, name='recipe'),
     
 ]
