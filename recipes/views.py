5c661b0acb83b63faff30e1191baa4169153ac82
Raimundo Neto <rfmeloneto@gmail.com>
2022-12-30 15:02:34 -0300

    preparation steps and view more button

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,12 @@ from .util import recipe_factory as rf
 
 def home(request):
     return render(request , 'recipes/pages/home.html', context={
-        'recipes':[rf.make_recipe() for _ in range(10)]
+        'recipes':[rf.make_recipe() for _ in range(10)], 
     })
 
 
 def recipes(request,id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': [rf.make_recipe()]
+        'recipe': [rf.make_recipe()],
+        'is_detail_page':True
     })
